517d5d152638923392f8f8d1d43f2141f0e775ad
Tanakorn Treechorvitaya <tanakorntreechorvitaya@Tanakorns-MBP.lan>
2023-03-10 00:04:59 +1100

    project 4 yumyumbites

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,28 @@
-import '@/styles/globals.css'
+import React, { useEffect, useState } from 'react';
+import { Layout } from '../components';
+
+import 'tailwindcss/tailwind.css'
+import '../styles/globals.scss';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
+
+// import React from 'react';
+
+// import '../styles/globals.scss';
+// import { Layout } from '../components';
+
+// function MyApp({ Component, pageProps }) {
+//   return (
+  //   <Layout>
+  //     <Component {...pageProps} />
+  //   </Layout>
+  // );
+// }
+
+// export default MyApp;
